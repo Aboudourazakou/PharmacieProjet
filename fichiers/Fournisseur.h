137cc39ae31d2ec7a9f6ce51863dc41c59b3a8f7
Aboudourazakou <tetereou2017@gmail.com>
2021-06-05 12:06:03 +0100

    Essai

--- fichiers/Fournisseur.h
+++ fichiers/Fournisseur.h
@@ -1,9 +1,15 @@
 #ifndef FOURNISSEUR_H
 #define FOURNISSEUR_H
+#include<iostream>
+using namespace std;
 
 class Fournisseur
 {
+	
 	public:
+		Fournisseur(){
+			cout<<"Je suis cree";
+		}
 	protected:
 };
 
--- fichiers/main.cpp
+++ fichiers/main.cpp
@@ -6,5 +6,7 @@
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 int main(int argc, char** argv) {
+	
+	Fournisseur f;
 	return 0;
 }
