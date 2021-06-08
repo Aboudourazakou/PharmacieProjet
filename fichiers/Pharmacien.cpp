ecf523dc88bd62e7d8958d7c49be8be1eedeb5c1
OUTNOUMAR Samia <81567582+Outnoumar300@users.noreply.github.com>
2021-06-07 22:05:43 -0700

    update classe

--- fichiers/Pharmacien.cpp
+++ fichiers/Pharmacien.cpp
@@ -1,3 +1,53 @@
 #include "Pharmacien.h"
+#include<fstream>
+int Pharmacien::getIdentifiant(){
+	return Identifiant;
+};
+string Pharmacien::getNom(){
+	return Nom;
+};
+string Pharmacien::getPrenom(){
+	return Prenom;
+};
+string Pharmacien::getMDP(){
+	return MDP;
+};
+string Pharmacien::getrole(){
+	return role;
+};
+void Pharmacien::setIdentifiant(int I){
+Identifiant=I;
+};
+void Pharmacien::setNom(string n){
+Nom=n;
+};
 
+void Pharmacien::setPrenom(string p){
+Prenom=p;
+}; 
+void Pharmacien::setMDP(string mdp){
+MDP=mdp;
+};
+void Pharmacien::setrole(string R){
+role=R;
+};
+void Pharmacien::afficher(){
+	cout<<"Identifiant: "<<Identifiant<<endl;
+	cout<<"Nom: "<<Nom<<endl;
+	cout<<"Prenom: "<<Prenom<<endl;
+	cout<<"MDP: "<<MDP<<endl;
+	cout<<"Role: "<<role<<endl;
+	
+};
+istream &operator>>(istream &p,Pharmacien&g){
+	ofstream file("Pharmaciens.txt",ios::app);
+	if(file.bad())cout<<"Erreur d'ouverture fichier"<<endl;
+	else{
+		file<<g.Identifiant<<"/"<<g.Nom<<"/"<<g.Prenom<<"/"<<g.MDP<<"/"<<g.role<<endl;
+	}
+	
+	
+	
+}
+ 
 
--- fichiers/main.cpp
+++ fichiers/main.cpp
@@ -2,11 +2,19 @@
 #include"Fournisseur.h"
 #include"Facture.h"
 #include"Medicament.h"
+#include"Pharmacien.h"
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 int main(int argc, char** argv) {
 	
-	Fournisseur f;
+    Pharmacien p;
+    p.setIdentifiant(786);
+    p.setMDP("62345678");
+    p.setNom("OUT");
+    p.setPrenom("Yahya");
+    p.setrole("Admin");
+    cin>>p;
+    
 	return 0;
 }
