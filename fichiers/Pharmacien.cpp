6c1074ebdd66f999a48c0d263e4583a166128fb9
OUTNOUMAR Samia <81567582+Outnoumar300@users.noreply.github.com>
2021-06-07 18:50:26 -0700

    first

---
+++ fichiers/Pharmacien.cpp
@@ -0,0 +1,5 @@
+#include "Pharmacien.h"
+
+Pharmacien::Pharmacien()
+{
+}
---
+++ fichiers/Pharmacien.h
@@ -0,0 +1,20 @@
+#ifndef PHARMACIEN_H
+#define PHARMACIEN_H
+using namespace std;
+class Pharmacien
+{   private:
+	int Identifiant;
+	string Nom;
+	string Prenom;
+	string MDP;
+	public:
+		Pharmacien();
+		int get Identifiant();
+		get Nom();
+		get Prenom();
+		get MDP();
+		void set Identifiant(int)
+	protected:
+};
+
+#endif
